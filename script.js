2302b16ced699f1cc2807ef18a7e57f99c57b92d
BoyWhileCoding <89507875+TheAsmitKid@users.noreply.github.com>
2023-06-10 21:57:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function signIn() {
 		"client_id":"906808596422-9mu2o4r7op0jpg4j4ttc8kfd125tnc9d.apps.googleusercontent.com",
 		"redirect_uri":"https://oniechat.netlify.app/profile/",
 		"response_type":"token",
-		"scope":"https://www.googleapis.com/auth/drive",
+		"scope":"https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
 		"include_granted_scopes":'true',
 		"state":"pass-through-value"
 	}
