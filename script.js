a98e5614c9977a97e8cb730cecbe7f5f0014c709
BoyWhileCoding <89507875+TheAsmitKid@users.noreply.github.com>
2023-06-10 18:37:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function signIn() {
 	form.setAttribute('action',oauth2Endpoint)
 
 	let params = {
-		"client_id":"11639426413-3bu6jv20a3p67q3ec4q6ca8g5qu09hvc.apps.googleusercontent.com",
+		"client_id":"906808596422-9mu2o4r7op0jpg4j4ttc8kfd125tnc9d.apps.googleusercontent.com",
 		"redirect_uri":"https://oniechat.netlify.app/profile/",
 		"response_type":"token",
-		"scope":"https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
+		"scope":"https://www.googleapis.com/auth/drive",
 		"include_granted_scopes":'true',
 		"state":"pass-through-value"
 	}
