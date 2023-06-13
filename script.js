384a72e73a9220e6f13b3b9295637f47f0a72be1
BoyWhileCoding <89507875+TheAsmitKid@users.noreply.github.com>
2023-06-13 10:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var apiUrl = 'https://script.google.com/macros/s/AKfycbzNpNsuI-d_9EXhB5fyQEXnWpX_4sPvW6iBzakxmO95tBjmauhGrfvidQBES4ETC3Ab/exec';
+
+fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error: ' + response.status);
+    }
+  })
+  .then(function(data) {
+    console.log(data); // Process the returned data here
+  })
+  .catch(function(error) {
+    console.log('API Error:', error);
+  });
+
+
 const searchbar = document.querySelector(".search");
 const submitBtn = document.querySelector(".search-btn");
 const photoWrapper = document.querySelector(".photo-wrapper");
